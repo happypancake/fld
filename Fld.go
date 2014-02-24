00b1bb7f3fae3795f7a63e5679afb2b8e9652e20
Rinat Abdullin <rinat.abdullin@gmail.com>
2014-02-24 06:00:06 -0800

    long fld to go-logging

--- Fld.go
+++ Fld.go
@@ -1,33 +1,64 @@
 package fld
 
 import (
-	"fmt"
+	"flag"
 	"net"
+
+	"github.com/op/go-logging"
 )
 
 var (
 	Instance *Fld
+	address  *string = flag.String("logsd", "127.0.0.1:8127", "UDP endpoint for LogsD daemon")
 )
 
 type Fld struct {
-	outgoing chan string
-	address  string
+	outgoing chan []byte
 	conn     net.Conn
 }
 
-func init() {
-	Start("127.0.0.1:8127")
+func (fld *Fld) Log(level logging.Level, depth int, rec *logging.Record) error {
+
+	switch level {
+	case logging.CRITICAL:
+		return send(fld, "crit", rec.Formatted())
+	case logging.ERROR:
+		return send(fld, "err", rec.Formatted())
+	case logging.WARNING:
+		return send(fld, "warning", rec.Formatted())
+	case logging.NOTICE:
+		return send(fld, "notice", rec.Formatted())
+	case logging.INFO:
+		return send(fld, "info", rec.Formatted())
+	case logging.DEBUG:
+		return send(fld, "debug", rec.Formatted())
+	default:
+	}
+	panic("unhandled log level")
 }
 
-func Start(address string) {
-	Instance = &Fld{address: address, outgoing: make(chan string, 100000)}
-	Instance.connect()
+func init() {
+	start()
+}
 
+func start() {
+	Instance = &Fld{outgoing: make(chan []byte, 100000)}
 	go Instance.processOutgoing()
 }
 
+func send(fld *Fld, severity string, name string) error {
+	length := float64(len(Instance.outgoing))
+	capacity := float64(cap(Instance.outgoing))
+
+	if length < capacity*0.9 {
+		payload := severity + "|" + name
+		Instance.outgoing <- []byte(payload)
+	}
+	return nil
+}
+
 func (fld *Fld) connect() error {
-	conn, err := net.Dial("udp", fld.address)
+	conn, err := net.Dial("udp", *address)
 	if err != nil {
 		return err
 	}
@@ -38,38 +69,13 @@ func (fld *Fld) connect() error {
 
 func (fld *Fld) processOutgoing() {
 	for outgoing := range fld.outgoing {
-		if _, err := fld.conn.Write([]byte(outgoing)); err != nil {
+
+		if nil == fld.conn {
 			fld.connect()
 		}
-	}
-}
 
-func Info(entry string) {
-	payload := createPayload(entry, "info")
-	send(payload)
-}
-
-func Debug(entry string) {
-	payload := createPayload(entry, "debug")
-	send(payload)
-}
-func Warning(entry string) {
-	payload := createPayload(entry, "warning")
-	send(payload)
-}
-func Error(entry string) {
-	payload := createPayload(entry, "error")
-	send(payload)
-}
-func createPayload(name string, severity string) string {
-	return fmt.Sprintf("%s|%s", name, severity)
-}
-
-func send(payload string) {
-	length := float64(len(Instance.outgoing))
-	capacity := float64(cap(Instance.outgoing))
-
-	if length < capacity*0.9 {
-		Instance.outgoing <- payload
+		if _, err := fld.conn.Write(outgoing); err != nil {
+			fld.connect()
+		}
 	}
 }
