e0c499d26d137a11fc7bef316969282db6d748d2
Pieter Joost van de Sande <pj@born2code.net>
2014-03-05 14:32:10 +0100

    Switch to go-integrate lib for etcd watching

--- Fld.go
+++ Fld.go
@@ -1,25 +1,23 @@
 package fld
 
 import (
+	"fmt"
 	"net"
-	"time"
 
-	"github.com/coreos/go-etcd/etcd"
+	stdetcd "github.com/coreos/go-etcd/etcd"
+	"github.com/happypancake/go-integrate/etcd"
 	"github.com/op/go-logging"
 )
 
 var (
-	Instance      *Fld
-	address       string
-	outgoing      = make(chan []byte, 100000)
-	conn          net.Conn
-	addressConfig chan *etcd.Response
-	log           = logging.MustGetLogger("fld")
+	address   string
+	outgoing  = make(chan []byte, 100000)
+	conn      net.Conn
+	addresses = make(chan string)
+	log       = logging.MustGetLogger("fld")
 )
 
-type Fld struct{}
-
-func (fld *Fld) Log(level logging.Level, depth int, rec *logging.Record) error {
+func Log(level logging.Level, depth int, rec *logging.Record) error {
 
 	switch level {
 	case logging.CRITICAL:
@@ -39,44 +37,11 @@ func (fld *Fld) Log(level logging.Level, depth int, rec *logging.Record) error {
 	panic("unhandled log level")
 }
 
-func InitWithDynamicConfig(client *etcd.Client) {
-	addressConfig = make(chan *etcd.Response)
-
-	go watchConfiguration(client, "/logsd/address")
-	go processOutgoing()
+func InitWithDynamicConfig(client *stdetcd.Client, hostname string) {
+	key := fmt.Sprint("/%v/logsd/address", hostname)
+	go etcd.GetAndWatchStringValue(client, key, addresses, nil)
 }
 
-func getCurrentVersion(client *etcd.Client, key string) *etcd.Response {
-	for {
-		if resp, err := client.Get(key, false, false); err == nil {
-			// failed to fetch first value
-			return resp
-		} else {
-			time.Sleep(time.Second)
-		}
-	}
-}
-func watchForUpdates(client *etcd.Client, key string, index uint64) {
-
-	for {
-		if _, err := client.Watch(key, index, false, addressConfig, nil); err != nil {
-			toSleep := 5 * time.Second
-
-			log.Debug("error watching etcd for key %v: %v", key, err)
-			log.Debug("retry in %v", toSleep)
-			time.Sleep(toSleep)
-		}
-	}
-
-}
-
-func watchConfiguration(client *etcd.Client, key string) {
-
-	resp := getCurrentVersion(client, key)
-	addressConfig <- resp
-
-	watchForUpdates(client, key, resp.EtcdIndex)
-}
 func send(severity string, name string) error {
 
 	length := float64(len(outgoing))
@@ -115,12 +80,8 @@ func processOutgoing() {
 			if _, err := conn.Write(outgoing); err != nil {
 				connect()
 			}
-		case response := <-addressConfig:
-
-			if response.Node != nil && response.Node.Value != "" {
-				address = response.Node.Value
-				connect()
-			}
+		case address = <-addresses:
+			connect()
 		}
 	}
 }
